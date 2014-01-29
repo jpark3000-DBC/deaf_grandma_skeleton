19e5a6c32bdab95e9abb2ab65025d5001d92321c
Apprentice <apprentice@dbc-chicago-54.local>
2014-01-28 21:48:27 -0600

    added asynchronous functionality and ajax support

--- deaf_grandma_skeleton/app/controllers/index.rb
+++ deaf_grandma_skeleton/app/controllers/index.rb
@@ -1,16 +1,15 @@
 get '/' do
-  @grandma = params[:grandma]
-  # Look in app/views/index.erb
   erb :index
 end
 
+
+
 post '/grandma' do
+  # puts "#{params[:user_input]}"
   # "Implement the /grandma route yourself.<br>Params: <code>#{params.inspect}</code>"
   if params[:user_input] == params[:user_input].upcase
-  	@grandma = "Cool DUDE!"
+  	"Cool DUDE!"
   else
-  	@grandma = "Speak up Kiddo"
+  	"Speak up Kiddo"
   end
-
-  redirect to("/?grandma=#{@grandma}")
 end
